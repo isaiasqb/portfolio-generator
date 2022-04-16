4173e1ac593c25bde684a6f6df34dfccb490ba34
IsaiasQB <isaias.qb@hotmail.com>
2022-04-15 21:51:23 -0400

    updated template to validate about me section

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-const { clear } = require('console');
+const fs = require('fs');
 const inquirer = require('inquirer')
+const generatePage = require('./src/page-template.js');
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err =>{
-//     if (err) throw err;
-
-// console.log('Portfolio Complete! Checkout index.html to see the output!');
-// });
 const promptUser = () => {
 return inquirer .prompt([
         {   
@@ -144,5 +136,17 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err =>{
+            if (err) throw new Error(err);
+
+        console.log('Portfolio Complete! Checkout index.html to see the output!');
+        });
     });
+
+    const mockData = {
+        name: 'mockname',
+        github: 'mockgithub',
+        projects: []
+    }
