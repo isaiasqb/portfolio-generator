f05e0382066594c30e000d5ea6a9f4eb178d4443
IsaiasQB <isaias.qb@hotmail.com>
2022-04-16 16:33:24 -0400

    filtered the featured projects

--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,65 @@
+const generateProjects = projectsArr => {
+        // generate an array of just feature projects
+    const featuredProjects = projectsArr.filter(project => {
+        if(project.feature) {
+            return true;
+        } else {
+            return false;
+        }
+    });
 
+        //get an array of all non-featured projects
+    const nonFeaturedProjects = projectsArr.filter(project => {
+        if(!project.feature) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+        //create the html template of what a feature object will be //generates this section for each individual project
+    const featuredProjectHtmlArr = featuredProjects.map(
+        ({name, description, languages, link}) => {
+        return `
+        <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="portfolio-languages">
+                Built With:
+                ${languages.join(', ')}
+            </h5>
+            <p>${description}</p>
+            <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+        </div>
+        `; 
+    });
+
+        //create the html template of what a NON-feature object will be
+    const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(
+        ({name, description, languages, link}) => {
+        return `
+        <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="portfolio-languages">
+            Built With:
+            ${languages.join(', ')}
+            </h5>
+            <p>${description}</p>
+            <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+        </div>
+        `;
+    });
+
+
+    return `
+    <section class="my-3" id="portfolio">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+        <div class="flex-row justify-space-between">
+            ${featuredProjectHtmlArr.join('')}
+            ${nonFeaturedProjectHtmlArr.join('')}
+        </div>
+    </section>
+    `; // appends all the individual project section to the main project section
+}
 
 module.exports = templateData => {
         // destructure page by data sections
@@ -15,7 +76,7 @@ module.exports = templateData => {
             <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
             <p>${aboutText}</p>
         </section>
-        `
+        `;
     } //end of generateAbout
 
     return `
@@ -44,6 +105,8 @@ module.exports = templateData => {
       </header>
       <main class="container my-5">
         ${generateAbout()}  
+
+        ${generateProjects()}
       </main>
       <footer class="container text-center py-3">
         <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
